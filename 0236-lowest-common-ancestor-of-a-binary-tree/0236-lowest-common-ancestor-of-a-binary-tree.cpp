95f606257006922b75e0ab490260ff7fdac6e95c
Vanshika Sood <vanshikasood77@gmail.com>
2023-06-22 19:50:49 +0530

    Time: 22 ms (38.95%), Space: 14.5 MB (15.95%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,27 +9,18 @@
  */
 class Solution {
 public:
-    bool solve(TreeNode* root, vector<TreeNode*>& ans, TreeNode* k){
-        if (!root) return false;
-        ans.push_back(root);
-        if (root->val == k->val) return true;
-        if(solve(root->left,ans,k) || solve(root->right,ans,k)) return true;
-        ans.pop_back();
-        return false;
+    TreeNode* solve(TreeNode* root, TreeNode* p, TreeNode* q){
+        if (!root) return NULL;
+        if (root == p || root == q) return root;
+        TreeNode* l = solve(root->left,p,q);
+        TreeNode* r = solve(root->right,p,q);
+        if (l and r) return root;
+        if (root->left == NULL and root->right == NULL) return NULL;
+        return l == NULL ? r : l;
     }
     
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*>ans1, ans2;
-        solve(root,ans1,p);
-        solve(root,ans2,q);
-        int i = 0, j = 0;
-        while(i < ans1.size() and j < ans2.size()){
-            if (ans1[i] != ans2[j]){
-                break;
-            }
-            i++;
-            j++;
-        }
-        return ans1[i-1];
+        return solve(root,p,q);
+        
     }
 };
