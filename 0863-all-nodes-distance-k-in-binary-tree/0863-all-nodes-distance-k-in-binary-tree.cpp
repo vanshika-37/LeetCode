18937b35bc60b2f4a3ccc7061afdf4a63c5b4bc5
Vanshika Sood <vanshikasood77@gmail.com>
2023-06-26 20:10:09 +0530

    Time: 4 ms (79.40%), Space: 15.2 MB (13.31%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int n = 0;
+    void dfs(TreeNode* root, unordered_map<TreeNode*,TreeNode*>&mp){
+        if (!root) return;
+        if (root->left) mp[root->left] = root;
+        if (root->right) mp[root->right] = root;
+        n++;
+        dfs(root->left,mp);
+        dfs(root->right,mp);
+    }
+    
+    void solve(TreeNode* root,  unordered_map<TreeNode*,TreeNode*>&mp, int k, vector<int>&ans, unordered_map<TreeNode*,bool>&visited, int dist){
+        if (!root || visited[root]) return;
+        if (dist == k){
+            ans.push_back(root->val);
+            return;
+        }
+        visited[root] = true;
+        solve(root->left,mp,k,ans,visited,dist+1);
+        solve(root->right,mp,k,ans,visited,dist+1);
+        solve(mp[root],mp,k,ans,visited,dist+1);
+        
+    }
+    
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        unordered_map<TreeNode*,TreeNode*>mp;
+        mp[root] = NULL;
+        dfs(root,mp);
+        int dist = 0;
+        vector<int>ans;
+        unordered_map<TreeNode*,bool>visited;
+        solve(target,mp,k,ans,visited,dist);
+        return ans;
+    }
+};
